76af3152f0c668b41fa19e4c9a293f49f6c3b450
Vilmar Jose Fagundes <165099836+VilmarFagundes@users.noreply.github.com>
2024-08-21 15:46:21 -0300

    Update user_account.py
    adição atributo idade no usuario, criação dos construtores text, post comentario

--- app/models/user_account.py
+++ app/models/user_account.py
@@ -1,6 +1,26 @@
 
 
 class UserAccount():
-    def __init__(self, username, password):
+    def __init__(self, username, password,age):
         self.username= username
         self.password= password
+        self.age = age
+
+class text():
+    def __init__(self,autor,titulo,conteudo):
+        self.autor = autor
+        self.titulo = titulo
+        self.conteudo = conteudo
+        
+        
+class post(text):
+        def __init__(self, autor, titulo, conteudo,data, curtidas):
+             super().__init__(autor, titulo, conteudo)
+             self.data = data
+             self.curtidas = curtidas
+             
+class comentario(text):
+        def __init__(self, autor, titulo, conteudo, verificado):
+            super().__init__(autor, titulo, conteudo)
+            self.verificado = verificado
+            
